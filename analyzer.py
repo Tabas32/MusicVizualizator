7ce8161361478eb60e37533e4b27f4c44a1456e0
Marián Sabat <marian.sabat@centrum.sk>
2017-10-29 20:17:27 +0100

    saving mfcc data to mfcc.npy file

--- analyzer.py
+++ analyzer.py
@@ -1,22 +1,48 @@
 import matplotlib.pyplot as plt
 import librosa, librosa.display
 from IPython.display import display, Audio
+from os import listdir, path
+import numpy as np
 
-# 1. Get the file path to the included audio example
-sample = "samples/sample0.wav"
+#open if exist mfcc.npy
+mfccFilename = "mfcc.npy"
+if path.isfile(mfccFilename):
+    print("Opening " + mfccFilename)
+    mfccList = list(np.load(mfccFilename))
+else:
+    print(mfccFilename + " not found, creating new")
+    mfccList = []
 
-# 2. Load the audio as a waveform `y`
-#    Store the sampling rate as `sr`
-y, sr = librosa.load(sample)
+#open if exist samples.npy
+samplesFilename = "samples.npy"
+if path.isfile(samplesFilename):
+    print("Opening " + samplesFilename)
+    samplesList = list(np.load(samplesFilename))
+else:
+    print(samplesFilename + " not found, creating new")
+    samplesList = []
+    
 
-S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
-log_S = librosa.logamplitude(S, ref_power=np.max)
-mfcc = librosa.feature.mfcc(S=log_S, sr=sr, n_mfcc=13)
+for filename in listdir("samples"):
+    sample = "samples/" + filename
+    y, sr = librosa.load(sample)
+
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
+    log_S = librosa.logamplitude(S, ref_power=np.max)
+    mfcc = librosa.feature.mfcc(S=log_S, sr=sr, n_mfcc=13)
+
+    if filename not in samplesList:
+        print("Saving " + filename + " data")
+        mfccList.append(mfcc)
+        samplesList.append(filename)
 
 
-plt.figure()
-librosa.display.specshow(mfcc, x_axis='time')
-plt.colorbar()
-plt.title('Monophonic')
+np.save(mfccFilename, mfccList)
+np.save(samplesFilename, samplesList)
 
-plt.show()
+#plt.figure()
+#librosa.display.specshow(mfcc, x_axis='time')
+#plt.colorbar()
+#plt.title('Monophonic')
+
+#plt.show()
