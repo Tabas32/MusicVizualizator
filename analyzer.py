b099f328103b1325eb69a1a629753e8f37f9246c
Marián Sabat <marian.sabat@centrum.sk>
2017-11-10 10:39:16 +0100

    features extracted, but not saved properly

--- analyzer.py
+++ analyzer.py
@@ -3,7 +3,6 @@ import librosa, librosa.display
 from IPython.display import display, Audio
 from os import listdir, path
 import numpy as np
-from matplotlib.mlab import PCA
 
 #open if exist mfcc.npy
 mfccFilename = "mfcc.npy"
@@ -27,13 +26,13 @@ else:
 for filename in listdir("samples"):
     sample = "samples/" + filename
     y, sr = librosa.load(sample)
+    # Mean amplitude (i gues) print(np.mean(np.absolute(y)))
 
     # MFCC
-    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128)
+    S = librosa.feature.melspectrogram(y=y, sr=sr, n_mels=128, hop_length=22050, n_fft=22050)
     log_S = librosa.logamplitude(S, ref_power=np.max)
-    mfcc = librosa.feature.mfcc(S=log_S, sr=sr, n_mfcc=13)
-
-    #TODO:print(PCA(mfcc))
+    mfcc = librosa.feature.mfcc(S=log_S, sr=sr, n_mfcc=20)
+    mfcc_1d_vector = mfcc.flatten()
 
     # TEMPO
     onset_env = librosa.onset.onset_strength(y, sr=sr)
@@ -46,12 +45,16 @@ for filename in listdir("samples"):
     # ZERO CROSSING RATE
     zcr = librosa.feature.zero_crossing_rate(y, frame_length=22050, hop_length=22050)
     total_zc = sum(librosa.core.zero_crossings(y))
-    print(zcr)
-    print(total_zc)
+    #print(zcr)
+    #print(total_zc)
 
     if filename not in samplesList:
         print("Saving " + filename + " data")
-        mfccList.append(mfcc)
+        mfccList.append(mfcc_1d_vector)
+        mfccList.append(zcr)
+        mfccList.append(tempo)
+        mfccList.append(cent_mean)
+        mfccList.append(total_zc)
         samplesList.append(filename)
 
 
